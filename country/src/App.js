c2249300963037a9524f6d257606383d48569545
Nithin <nithinappu963@gmail.com>
2024-05-09 23:04:10 +0530

    country flag

---
+++ country/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Flag from './Components/Flag';
+
+function App() {
+  return (
+    <div className="App">
+      <Flag/>
+    </div>
+  );
+}
+
+export default App;
---
+++ country/src/Components/Flag.jsx
@@ -0,0 +1,58 @@
+import { useEffect,useState } from "react"
+
+const Tile = ({ flagUrl, name, altFlag }) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "10px",
+          padding: "10px",
+          border: "1px solid black",
+          borderRadius: "8px",
+          flexDirection: "column",
+          width: "200px",
+        }}
+      >
+        <img
+          src={flagUrl}
+          alt={altFlag}
+          style={{ width: "100px", height: "100px" }}
+        />
+        <h2>{name}</h2>
+      </div>
+    );
+  };
+export default function Flag(){
+  const [flag , setFlag] = useState();
+  const fetchFlag = async () =>{
+    const res = await fetch(`https://restcountries.com/v3.1/all`);
+    const data = await res.json();
+    console.log(data)
+    setFlag(data);
+  }
+  useEffect(() =>{
+    fetchFlag();
+  },[])
+    return(
+        <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          flexWrap: "wrap",
+        }}
+      >
+        {flag.map((country) => (
+          <Tile
+            key={country.cca3}
+            flagUrl={country.flags.png}
+            name={country.name.common}
+            altFlag={country.flags.alt}
+          />
+        ))}
+      </div>
+    )
+}

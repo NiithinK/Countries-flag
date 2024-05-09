408ecaeed630319bfb4daec451870229ba799f6d
Nithin <nithinappu963@gmail.com>
2024-05-09 23:09:47 +0530

    country flag 2

--- country/src/Components/Flag.jsx
+++ country/src/Components/Flag.jsx
@@ -1,64 +1,61 @@
-import { useEffect,useState } from "react"
+import { useState, useEffect } from "react";
 
 const Tile = ({ flagUrl, name, altFlag }) => {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "10px",
-          padding: "10px",
-          border: "1px solid black",
-          borderRadius: "8px",
-          flexDirection: "column",
-          width: "200px",
-        }}
-      >
-        <img
-          src={flagUrl}
-          alt={altFlag}
-          style={{ width: "100px", height: "100px" }}
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px",
+        padding: "10px",
+        border: "1px solid black",
+        borderRadius: "8px",
+        flexDirection: "column",
+        width: "200px",
+      }}
+    >
+      <img
+        src={flagUrl}
+        alt={altFlag}
+        style={{ width: "100px", height: "100px" }}
+      />
+      <h2>{name}</h2>
+    </div>
+  );
+};
+
+function Flag() {
+  const API_URL = "https://restcountries.com/v3.1/all";
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error happening: ", error));
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexWrap: "wrap",
+      }}
+    >
+      {countries.map((country) => (
+        <Tile
+          key={country.cca3}
+          flagUrl={country.flags.png}
+          name={country.name.common}
+          altFlag={country.flags.alt}
         />
-        <h2>{name}</h2>
-      </div>
-    );
-  };
-export default function Flag(){
-  const [flag , setFlag] = useState();
-  const fetchFlag = async () =>{
-    const res = await fetch(`https://restcountries.com/v3.1/all`);
-    const data = await res.json();
-    console.log(data)
-    setFlag(data);
-  }
-  useEffect(() =>{
-    fetchFlag();
-  },[])
-    return(
-        <>
-        <div>
-            <h1>Flag</h1>
-        </div>
-        <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          flexWrap: "wrap",
-        }}
-      > 
-     
-        {flag.map((country) => (
-          <Tile
-            key={country.cca3}
-            flagUrl={country.flags.png}
-            name={country.name.common}
-            altFlag={country.flags.alt}
-          />
-        ))}
-      </div>
-        </>
-    )
+      ))}
+    </div>
+  );
 }
+
+export default Flag;
